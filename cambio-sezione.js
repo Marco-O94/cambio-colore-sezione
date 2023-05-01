36708322dd971487e32f8d497bf57f86ea31b6c0
Marco Oliveri <marcooliveri94@gmail.com>
2023-05-01 16:57:47 +0200

    Update cambio-sezione.js

--- cambio-sezione.js
+++ cambio-sezione.js
@@ -1,8 +1,11 @@
+var sections = document.querySelectorAll('observed_sections');
+var section_plus = document.querySelectorAll('observed_sections_plus');
+
 window.addEventListener("scroll", function() {
 
  /* Da qui si regola l'ingresso animazione: 
- * Parte da un valore negativo di -189 per ora, per continuare
- * a togliere inserire valori negativi.
+ * Parte da un valore offsetTop della prima sezione
+ * 
  */
  let sectionsHeight = sections[0].offsetTop;
  let optionalSec = 0;
@@ -10,7 +13,7 @@ window.addEventListener("scroll", function() {
 	/* */
  for(let i = 0; i < sections.length; i++) {
 	 // da qui si modifica la condizione dell'optionalSec se in futuro si devono aggiungere sezioni con lo stesso colore a seguire
-     i === 3 ? optionalSec = section_plus.offsetHeight : optionalSec = 0;
+     i === 3 ? optionalSec = section_plus[i].offsetHeight : optionalSec = 0;
      if(window.scrollTop() >= sectionsHeight && window.scrollTop() < sectionsHeight + sections[i].offsetHeight + optionalSec) {
        (i + 1) % 2 === 0 ? change_state_1() : change_state_2();
  }
